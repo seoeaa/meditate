7089208a8201899bfdcf20ca43f9ec98638d2cff
seoeaa <seoinetru@gmail.com>
2022-02-04 07:15:31 +0300

    add database
    add database

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'db-mysql-fra1-22248-do-user-10783224-0.b.db.ondigitalocean.com'),
+      host: env('DATABASE_HOST', 'db-mysql-do-user-10783224-0.b.db.ondigitalocean.com'),
       port: env.int('DATABASE_PORT', 25060),
       database: env('DATABASE_NAME', 'defaultdb'),
       user: env('DATABASE_USERNAME', 'doadmin'),
-      password: env('DATABASE_PASSWORD', 'szESQOP1JQNY0sQw'),
+      password: env('DATABASE_PASSWORD', 'reXIeASLBngWcIyu'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
