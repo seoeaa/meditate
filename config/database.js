fbb92996841859748a9ae1fd2a322052d6b67fca
seoeaa <seoinetru@gmail.com>
2022-02-04 12:32:21 +0300

    TEST
    обновил конфиги

--- config/database.js
+++ config/database.js
@@ -1,16 +1,22 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
-    connection: {
-      host: env('DATABASE_HOST', 'db-mysql-do-user-10783224-0.b.db.ondigitalocean.com'),
-      port: env.int('DATABASE_PORT', 25060),
-      database: env('DATABASE_NAME', 'defaultdb'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', '0bj5RNVkD7cO3vv7'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env('DATABASE_HOST'),
+        port: env('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
+      options: {},
     },
-    debug: false,
   },
 });
+
+
--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6763406e0177c5b2acb7163a87c8bdea'),
+    },
   },
+  cron: {
+    enabled: true
+  }
 });
